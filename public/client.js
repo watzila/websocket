99ffca5d70b0ac6c6e95dc4a6e106bfee36f91c2
DevInsights.tw <61079322+watzila@users.noreply.github.com>
2025-11-20 16:30:47 +0800

    Remove TURN server configuration from client.js
    Removed TURN server configuration to simplify connection setup.

--- public/client.js
+++ public/client.js
@@ -22,17 +22,6 @@ class Chat {
             "stun:stun1.l.google.com:19302",
             "stun:stun2.l.google.com:19302"
           ]
-        },
-        // 加入 TURN 伺服器以改善非區網連線
-        {
-          urls: "turn:openrelay.metered.ca:80",
-          username: "openrelayproject",
-          credential: "openrelayproject"
-        },
-        {
-          urls: "turn:openrelay.metered.ca:443",
-          username: "openrelayproject",
-          credential: "openrelayproject"
         }
       ]
     };
